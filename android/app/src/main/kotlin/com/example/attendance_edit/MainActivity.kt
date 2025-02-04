348fb584f45f8071006c00490549ebe53ec43ab7
VallianXS <valliansayogach14@gmail.com>
2025-02-04 08:33:18 +0700

    starter

---
+++ android/app/src/main/kotlin/com/example/attendance_edit/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.attendance_edit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyCGf36wBi26jLLofS1_TfuzZZR58jywRyI", // Current Key
+      appId: "1:369074005771:android:1e2e5bcdd543ec587c640e", // mobilesdk_app_id
+      messagingSenderId: "369074005771", // Project Number
+      projectId: "attendance-edit-d83fa", // Project ID
+    )
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
